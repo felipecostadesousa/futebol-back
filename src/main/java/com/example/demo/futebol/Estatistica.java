34d73564af340a23f1dc7e7f822ddb2b13be1003
aadriellyy <limaadrielly214@gmail.com>
2025-01-16 20:20:15 -0300

    :sparkles: feat: continuação das entidades

---
+++ src/main/java/com/example/demo/futebol/Estatistica.java
@@ -0,0 +1,87 @@
+package com.example.demo.futebol;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="estatistica", schema = "public")
+public class Estatistica {
+
+    @Id
+    @Column(name="id", nullable = false)
+    private Integer id;
+
+    @Column(name="quantidade_jogos_jogados", nullable = false)
+    private Integer jogos;
+
+    @Column(name="quantidade_gols_marcados", nullable = false)
+    private Integer gols;
+
+    @Column(name="quantidade_assistencias_gols", nullable = false)
+    private Integer assistencias;
+
+    @OneToOne
+    @JoinColumn(name="fk_jogador", referencedColumnName = "id_jogador")
+    private Jogador jogador;
+
+    public Estatistica(){
+
+    }
+
+    public Estatistica(Integer id, Integer jogos, Integer gols, Integer assistencias, Jogador jogador){
+        this.id = id;
+        this.jogos = jogos;
+        this.gols = gols;
+        this.assistencias = assistencias;
+        this.jogador = jogador;
+    }
+
+    public Estatistica(Integer jogos, Integer gols, Integer assistencias, Jogador jogador){
+        this.jogos = jogos;
+        this.gols = gols;
+        this.assistencias = assistencias;
+        this.jogador = jogador;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(Integer jogos) {
+        this.jogos = jogos;
+    }
+
+    public Integer getGols() {
+        return gols;
+    }
+
+    public void setGols(Integer gols) {
+        this.gols = gols;
+    }
+
+    public Integer getAssistencias() {
+        return assistencias;
+    }
+
+    public void setAssistencias(Integer assistencias) {
+        this.assistencias = assistencias;
+    }
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    
+
+}
---
+++ src/main/java/com/example/demo/futebol/Titulo.java
@@ -0,0 +1,44 @@
+package com.example.demo.futebol;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="titulo", schema = "public")
+public class Titulo {
+
+    @Id
+    @Column(name="id", nullable = false)
+    private Integer id;
+
+    @Column(name="nome", nullable = false)
+    private String nome;
+
+    public Titulo() {
+    }
+
+    public Titulo(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
