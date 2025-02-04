5201cc4cc61e9369fec7ebf68f6a14f86f6ada87
aadriellyy <limaadrielly214@gmail.com>
2025-02-04 15:20:15 -0300

    feat: crud

--- src/main/java/com/example/demo/futebol/rest/Arbitro/ArbitroRequest.java
+++ src/main/java/com/example/demo/futebol/rest/Arbitro/ArbitroRequest.java
@@ -7,7 +7,6 @@ import com.example.demo.futebol.rest.ValidOnCreation;
 import com.example.demo.futebol.rest.ValidOnUpdate;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;;
 
 public class ArbitroRequest {
